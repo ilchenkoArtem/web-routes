d5ba19d1480853e976354bae7ad45e150e480158
ilchenkoArtem <artem.ilchenko@711media.de>
2024-03-27 00:57:42 +0200

    refactor: rename nested config

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export { createRoutes as CreateNestedRoutes } from './nested-routes';
+export { createRoutes as createNestedRoutes } from './nested-routes';
 export type { RoutesConfig as NestedRoutesConfig } from './nested-routes/types';
