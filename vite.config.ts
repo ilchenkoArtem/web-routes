d8bf97295eeb752e65d0f0ce005681a56d3cb654
ilchenkoArtem <artem.ilchenko@711media.de>
2024-04-09 08:24:29 +0300

    refactor: vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/guide/build.html#library-mode
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
@@ -13,10 +13,9 @@ export default defineConfig(({ mode }) => ({
     },
   },
   test: { environment: 'jsdom' },
-  define: process.env.VITEST ? {} : { global: 'window' },
   plugins: [
     dts({
       insertTypesEntry: true,
     }),
   ],
-}));
+});
