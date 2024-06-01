a525a325b26a5093cf6a2f2d80aad13e37947eda
ilchenkoArtem <artem.ilchenko@711media.de>
2024-06-01 20:05:39 +0300

    refactor: update create routes

---
+++ src/nested-routes/create-routes.ts
@@ -0,0 +1,76 @@
+import {
+  RouteBase,
+  Routes,
+  RoutesConfig,
+  RouteWithParams,
+  RouteWithQuery,
+  RouteWithQueryAndParams,
+} from './types';
+
+import {
+  replaceParams,
+  addQuery,
+  mapRouteQueryToConfigQuery,
+  mergeUrl,
+  configQueryToRouteQuery,
+} from './utils';
+
+export type RouteTypes =
+  | RouteBase
+  | RouteWithQuery<any>
+  | RouteWithParams<any>
+  | RouteWithQueryAndParams<any>;
+
+export interface UrlOptions {
+  query?: Record<string, string>;
+  params?: Record<string, string | number>;
+  withBackTo?: boolean;
+}
+
+export const createRoutes = <T extends RoutesConfig>(config: T): Routes<T> => {
+  const buildRoutes = (config: RoutesConfig, parentUrl: string = '/') => {
+    let routes = {} as Record<string, RouteTypes>;
+
+    Object.entries(config).forEach(([routeId, routeConfig]) => {
+      let url: string = mergeUrl(parentUrl, routeConfig.url);
+      const configQueryObject = configQueryToRouteQuery(routeConfig.query);
+
+      routes[routeId] = {
+        $url: (options: UrlOptions = {}) => {
+          if (!options) return url;
+
+          if (options.params) {
+            url = replaceParams(url, options.params);
+          }
+
+          if (routeConfig.query && options.query) {
+            const mappedQuery = mapRouteQueryToConfigQuery(
+              options.query,
+              configQueryObject,
+            );
+            url = addQuery(url, mappedQuery);
+          }
+
+          const withBackTo = options.withBackTo ?? !!routeConfig.withBackTo;
+          if (withBackTo && typeof window !== 'undefined') {
+            url = addQuery(url, {
+              backTo: window.location.pathname + window.location.search,
+            });
+          }
+
+          return url;
+        },
+        ...(routeConfig.query && {
+          $query: configQueryObject,
+        }),
+        ...(routeConfig.children && {
+          ...buildRoutes(routeConfig.children, url),
+        }),
+      };
+    });
+
+    return routes;
+  };
+
+  return buildRoutes(config, '/') as Routes<T>;
+};
